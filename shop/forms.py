4f4fc709f17758cdaa9899e28293a5e3ae00d5aa
alinaapopescu <popescu.alina79@yahoo.com>
2025-04-10 11:41:02 +0300

    Add HTML injection, comment section to products and particially auth functionalities

--- shop/forms.py
+++ shop/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Order
+from .models import Order, Comment
+
 
 class CheckoutForm(forms.ModelForm):
     class Meta:
@@ -14,3 +15,8 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -24,3 +27,13 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.product.name}"
